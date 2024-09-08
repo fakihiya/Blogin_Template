5ef24d377a353a4a8ef52fd47749396a2a7b74b4
fakihiya <yasinfakihi21@gmail.com>
2024-09-08 16:11:28 +0100

    initial commit

--- routes/web.js
+++ routes/web.js
@@ -1,4 +1,21 @@
+
+
 const express = require('express');
+
 const router = express.Router();
 
+router.get('/', (req, res) => {
+    res.render('commente')
+});
+
+
+router.post('/', (req, res) => {
+    const { comment } = req.body;
+    res.send(`New comment added: ${comment}`);
+});
+
+
+
 module.exports = router;
+
+
